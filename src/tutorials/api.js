384d218e58a8bf8a8aa41c9dace5f36f7fb7a424
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-11 00:31:04 +0400

    [ADD] status codes

--- src/tutorials/api.js
+++ src/tutorials/api.js
@@ -2,14 +2,19 @@
 const getResource = async (url) => {
     const res = await fetch(url)
 
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, recieved ${res.status}`)
+    }
     const body = await res.json()
 
     return body
 }
 
 
-getResource("https://swapi.dev/api/films/1/").then((body) => {
+getResource("https://swapi.dev/api/films/1378979879/").then((body) => {
     console.log(body)
+}).catch((error) => {
+    console.log("Could not fetch. ", error)
 })
 
 
