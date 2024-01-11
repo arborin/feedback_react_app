dd7e72889ea5d78bca6ee8abbd0a41c51d9f1357
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-11 13:01:10 +0400

    [ADD] json placeholder api

--- src/tutorials/api.js
+++ src/tutorials/api.js
@@ -1,20 +1,38 @@
 
-const getResource = async (url) => {
-    const res = await fetch(url)
+class ApiService {
+    _apiBase = "https://jsonplaceholder.org"
 
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url}, recieved ${res.status}`)
+    async getResource(url) {
+        const apiUrl = `${this._apiBase}${url}`
+        console.log(apiUrl)
+
+        const res = await fetch(apiUrl)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${apiUrl}, recieved ${res.status}`)
+        }
+        const body = await res.json()
+
+        return body
     }
-    const body = await res.json()
 
-    return body
+    getPosts() {
+        return this.getResource(`/posts`)
+    }
+
+    getPost(id) {
+        return this.getResource(`/posts/${id}`)
+    }
 }
 
 
-getResource("https://swapi.dev/api/films/1378979879/").then((body) => {
-    console.log(body)
-}).catch((error) => {
-    console.log("Could not fetch. ", error)
-})
+const obj = new ApiService()
 
+obj.getPosts().then((posts) => {
+    posts.forEach(post => {
+        console.log(post.title);
+        console.log(post.url);
+        console.log("========================================")
+    });
+})
 
