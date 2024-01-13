043e3858d6a9cd3edfe5351f748edc002ce3bb0b
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-13 23:39:19 +0400

    [ADD] loading

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ApiService from "../../services/api-services";
+import Spinner from "../spinner";
 class Posts extends React.Component {
 
 
@@ -7,6 +8,7 @@ class Posts extends React.Component {
     state = {
         name: null,
         posts: null,
+        loading: true
     }
 
     apiService = new ApiService();
@@ -14,7 +16,7 @@ class Posts extends React.Component {
 
     getPostList() {
         this.apiService.getPosts().then((posts) => {
-            this.setState({ posts: posts })
+            this.setState({ posts: posts, loading: false })
         })
     }
 
@@ -26,38 +28,47 @@ class Posts extends React.Component {
     render() {
 
 
-        const { posts, name } = this.state;
+        const { posts, name, loading } = this.state;
         console.log(posts)
         return (
             <>
-                <h4>{name}</h4>
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {posts && posts.map((post, inx) => {
-                            return (
-                                <tr key={inx} className="table-active">
-                                    <th scope="row">{post.id}</th>
-                                    <td>{post.title}</td>
-                                    <td>{post.category}</td>
-                                    <td><button className="btn btn-primary btn-sm">show</button></td>
-                                </tr>
-                            )
-                        })}
-
-                    </tbody>
-                </table>
+                <PostList name={name} posts={posts} loading={loading} />
             </>
         )
     }
+}
+
+const PostList = ({ name, loading, posts }) => {
+    return (
+        <>
+            <h4>{name}</h4>
+            {loading && <Spinner />}
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {posts && posts.map((post, inx) => {
+                        return (
+                            <tr key={inx} className="table-active">
+                                <th scope="row">{post.id}</th>
+                                <td>{post.title}</td>
+                                <td>{post.category}</td>
+                                <td><button className="btn btn-primary btn-sm">show</button></td>
+                            </tr>
+                        )
+                    })}
+
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default Posts
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './spinner.css'
+
+class Spinner extends React.Component {
+
+    render() {
+        return (
+            <div className="loadingio-spinner-spin-tn4hrgmgw7e"><div className="ldio-axa1iz7vw9">
+                <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+            </div></div>
+        )
+    }
+
+}
+
+export default Spinner
+
+
