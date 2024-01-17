85e8c3a57324e584c5b341628e2c02f808618e97
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-18 00:14:43 +0400

    [ADD] useContext

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import { useState } from "react"
+import React from "react";
+import { useState, useContext } from "react"
+
+const MyContext = React.createContext();
 
 export default function App() {
   return (
-    <>
+    <MyContext.Provider value="HELLO">
       <div className="container">
         <h1>Hooks</h1>
-        <HookSwitch />
+        <Child />
       </div>
-    </>
+    </MyContext.Provider>
   )
 }
 
-
-const HookSwitch = () => {
-
-  const [color, setColor] = useState('white')
-  const [fontSize, setFontSize] = useState(14)
-
+const Child = () => {
+  const value = useContext(MyContext);
   return (
-    <div style={{ background: color, fontSize: fontSize }}>
-      <p>some text for fontsize test</p>
-      <button className="btn btn-primary" onClick={() => setColor('white')}>Light</button>
-      <button className="btn btn-warning" onClick={() => setColor('black')}>Dark</button>
-      <button className="btn btn-warning" onClick={() => setFontSize(fontSize + 2)}>+</button>
-      <button className="btn btn-warning" onClick={() => setFontSize(fontSize - 2)}>-</button>
-    </div>
+    <p>{value}</p>
   )
 }
+
+
+// const HookSwitch = () => {
+
+//   const [color, setColor] = useState('white')
+//   const [fontSize, setFontSize] = useState(14)
+
+//   return (
+//     <div style={{ background: color, fontSize: fontSize }}>
+//       <p>some text for fontsize test</p>
+//       <button className="btn btn-primary" onClick={() => setColor('white')}>Light</button>
+//       <button className="btn btn-warning" onClick={() => setColor('black')}>Dark</button>
+//       <button className="btn btn-warning" onClick={() => setFontSize(fontSize + 2)}>+</button>
+//       <button className="btn btn-warning" onClick={() => setFontSize(fontSize - 2)}>-</button>
+//     </div>
+//   )
+// }
