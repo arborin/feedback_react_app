2131a46ffd26241eb7a74066eb431af70ea66b21
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-18 22:25:08 +0400

    [ADD] react router navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 export default function App() {
   return (
     <div className="container">
-      <h1>React Router</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
   )
 }
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+      <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const About = () => {
+  return <h1>About</h1>
+}
+
+const Contact = () => {
+  return <h1>Contact</h1>
+}
+
+
 
