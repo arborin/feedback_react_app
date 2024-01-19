78e4a9716cf3d34f735153f98438ba967e3bc9e1
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-19 23:47:02 +0400

    [ADD] error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useParams } from "react-router-dom/dist/umd/react-router-dom.development"
+import { redirect, useNavigate, useParams } from "react-router-dom/dist/umd/react-router-dom.development"
 
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+
+  const onLogin = () => {
+    setIsLoggedIn(true)
+  }
+  const onLogOut = () => {
+    setIsLoggedIn(false)
+  }
+
   return (
     <div className="container">
       <BrowserRouter>
@@ -11,9 +23,9 @@ export default function App() {
           <Route path="/" exect element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/service/:id/:name" element={<Service />} />
-          <Route path="/dashboard" isLoggedIn={false}
-            element={<Dashboard />} />
-          <Route path="/Login" element={<Login onLogin={() => console.log("CLICK LOCIN")} isLoggedIn={false} />} />
+          <Route path="/dashboard" element={<Dashboard isLoggedIn={isLoggedIn} />} />
+          <Route path="/Login" element={<Login onLogin={onLogin} isLoggedIn={isLoggedIn} onLogOut={onLogOut} />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -64,20 +76,37 @@ const Service = () => {
 }
 
 
-const Login = ({ isLoggedIn, onLogin }) => {
+const Login = ({ isLoggedIn, onLogin, onLogOut }) => {
   return (
     <>
       <p>Login to see secret page</p>
       <button className="btn btn-outline-warning" onClick={onLogin}>Login</button>
+      <button className="btn btn-outline-danger" onClick={onLogOut}>Log Out</button>
     </>
   )
 }
 
 const Dashboard = ({ isLoggedIn }) => {
+  console.log(isLoggedIn)
   if (isLoggedIn) {
     return <h1>Dashboard</h1>
   }
   return <h1>Access Denied!</h1>
+}
+
+const Error = () => {
+  let navigate = useNavigate();
+
+  const goHome = () => {
+    return navigate("/");
+  }
+
+  return (
+    <>
+      <h1>Page not found!</h1>
+      <button className="btn btn-success" onClick={goHome}>Home</button>
+    </>
+  )
 }
 
 
