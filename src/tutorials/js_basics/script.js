1e5e8970ac35ba63c82a94de8c482b0fb31373b7
nika kobaidze <nika.kobaidze@gmail.com>
2024-01-23 18:22:56 +0400

    [add] import all

--- src/tutorials/js_basics/script.js
+++ src/tutorials/js_basics/script.js
@@ -1,4 +1,9 @@
 console.log("Script load")
 
-import { key, name } from './util.js'
-console.log(key, name)
+// import { key, name } from './util.js'
+// console.log(key, name)
+
+import * as util from './util.js'
+
+console.log(util.key)
+console.log(util.name)
