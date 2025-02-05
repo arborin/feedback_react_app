9890b565f557d6a4eef4d1e1575112622fc6df26
nika kobaidze <nika.kobaidze@gmail.com>
2025-02-05 10:20:51 +0400

    clear

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
-
-
 export default function App() {
 
-    // if (loading) return <h1>Loading...</h1>
-
-    const [feedback, setFeedback] = useState(FeedbackData)
 
 
-    return (
-        <>
-            <Header text='Feedback UI' />
-            <div className="container">
-                <FeedbackList feedback={feedback} />
-            </div>
-        </>
-    )
+    export const ThemeProvider = () => {
+
+        return (
+            <h1>Start</h1>
+        );
+    };
 }
-
 
 
 
